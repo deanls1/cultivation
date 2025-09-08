7b0613793085c50c7a488020b80ee4ef194e36bb
wpg <deanqs@qq.com>
2025-09-08 18:17:23 +0800

    feat(i18n): Localize fighting page table headers

--- src/Pages/FightingPage.tsx
+++ src/Pages/FightingPage.tsx
@@ -10,21 +10,23 @@ import {
 } from "@mui/material";
 import { GameContext } from "GameEngine";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import EnemyRow from "./FightingPage/EnemyRow";
 
 export default function FightingPage() {
   const { enemies } = React.useContext(GameContext);
+  const { t } = useTranslation();
   return (
     <Box marginTop={2} marginLeft={2}>
       <TableContainer sx={{ width: 800, height: 600, overflow: "auto" }}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>name</TableCell>
-              <TableCell>health</TableCell>
-              <TableCell>attack</TableCell>
-              <TableCell>defence</TableCell>
-              <TableCell>healthRegen</TableCell>
+              <TableCell>{t("name")}</TableCell>
+              <TableCell>{t("health")}</TableCell>
+              <TableCell>{t("attack")}</TableCell>
+              <TableCell>{t("defence")}</TableCell>
+              <TableCell>{t("healthRegen")}</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
